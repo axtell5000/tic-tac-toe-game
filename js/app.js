393f78d97e48e6dd3b64eb4fc7a7919790cb2174
Stephen Axtell <axtell.stephen@gmail.com>
2023-02-05 15:58:16 +0200

    some configuration area styling plus logic for entering name and which player and opening and closing modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
+// Okay to do this in one file, the rest of the app will have access to it
+let editedPlayer = 0;
+
+const players = [
+  { name: "", symbol: "X" },
+  { name: "", symbol: "O" },
+];
+
 const playerConfigOverlayEle = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formEle = document.querySelector("form");
+const errorsOutput = document.getElementById("config-errors");
 
 const editPlayerOneBtnEle = document.getElementById("edit-player1-details");
 const editPlayerTwoBtnEle = document.getElementById("edit-player2-details");
@@ -9,3 +19,5 @@ editPlayerOneBtnEle.addEventListener("click", openPlayerConfig);
 editPlayerTwoBtnEle.addEventListener("click", openPlayerConfig);
 cancelConfigBtn.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formEle.addEventListener("submit", savePlayerConfig);
--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = parseInt(event.target.dataset.playerid); // targetting data item called playerid
   playerConfigOverlayEle.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -6,4 +7,26 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayEle.style.display = "none";
   backdropElement.style.display = "none";
+  formEle.firstElementChild.classList.remove("error");
+  errorsOutput.textContent = "";
+  formEle.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target); // form api, which constructs an object like item out of the fields. Name attribute is key
+  const enteredPlayerName = formData.get("playername").trim(); // getting the value using the name attribute
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutput.textContent = "Please enter a valid name!";
+    return;
+  }
+  const updatedPlayerDataElement = document.getElementById(
+    "player" + editedPlayer + "-data"
+  ); // getting player based on data item then get by ID
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
