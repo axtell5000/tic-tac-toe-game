7292105d3ad0513a5646ab405e19e4b571a73f4e
Stephen Axtell <axtell.stephen@gmail.com>
2023-02-08 14:00:59 +0200

    setting up game logic and determine who won the game

--- js/app.js
+++ js/app.js
@@ -1,24 +1,31 @@
 // Okay to do this in one file, the rest of the app will have access to it
+// used to help track who won the game
 const gameData = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
 ];
 
+// Various flagers and counters
 let editedPlayer = 0;
 let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
 
 const players = [
   { name: "", symbol: "X" },
   { name: "", symbol: "O" },
 ];
 
+// Selecting DOM elements to work with in the future
 const playerConfigOverlayEle = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formEle = document.querySelector("form");
 const errorsOutput = document.getElementById("config-errors");
 const gameAreaEle = document.getElementById("active-game");
 const activePlayerNameEle = document.getElementById("active-player-name");
+const gameOverEle = document.getElementById("game-over");
+const gameBoardElement = document.getElementById("game-board");
 
 const editPlayerOneBtnEle = document.getElementById("edit-player1-details");
 const editPlayerTwoBtnEle = document.getElementById("edit-player2-details");
--- js/game.js
+++ js/game.js
@@ -1,8 +1,31 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverEle.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>';
+  gameOverEle.style.display = "none";
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = "";
+      gameBoardItemElement.classList.remove("disabled");
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGane() {
   if (players[0].name === "" || players[1].name === "") {
     alert("Please enter both players name please");
     return;
   }
+
+  resetGameStatus();
+
   activePlayerNameEle.textContent = players[activePlayer].name;
   gameAreaEle.style.display = "block";
 }
@@ -17,10 +40,11 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
+  // to make it work with the array, from data attribue to the array
   const selectedColumn = event.target.dataset.col - 1;
   const selectedRow = event.target.dataset.row - 1;
 
-  if (gameData[selectedRow][selectedColumn] > 0) {
+  if (gameData[selectedRow][selectedColumn] > 0 || gameIsOver) {
     alert("Please select an empty field");
     return;
   }
@@ -29,7 +53,71 @@ function selectGameField(event) {
   event.target.classList.add("disabled");
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  console.log(gameData);
+  const winnerId = checkForGameOver();
 
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
+
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // Checking the rows
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      (gameData[i][0] === gameData[i][1]) === 1 &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+  // Checking the columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Checking the diagonal values: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // Checking the diagonal values: Bottom left to top right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverEle.style.display = "block";
+
+  if (winnerId > 0) {
+    const winnerNmae = players[winnerId - 1].name;
+    gameOverEle.firstElementChild.firstElementChild.textContent = winnerNmae;
+  } else {
+    gameOverEle.firstElementChild.textContent = "It is a draw!";
+  }
 }
